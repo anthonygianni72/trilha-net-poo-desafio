84f488caa5408a4ff8abecc41860528543fbef86
Anthony Gianni <agab@cin.ufpe.br>
2024-01-03 15:54:33 -0300

    solução do desafio

--- Program.cs
+++ Program.cs
@@ -1,3 +1,33 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+class Program
+{
+    static void Main()
+    {
+        Nokia nokiaPhone = new Nokia("123456", "Nokia Model", "IMEI123", 64);
+
+        // Chamando métodos herdados da classe Smartphone nokia
+        nokiaPhone.Ligar();
+        nokiaPhone.ReceberLigacao();
+
+        // Chamando o método sobrescrito da classe Nokia
+        nokiaPhone.InstalarAplicativo("MeuApp");
+
+        // Você também pode acessar as propriedades herdados da classe Smartphone
+        Console.WriteLine($"Número: {nokiaPhone.Numero}, Modelo: {nokiaPhone.Modelo}, Memória: {nokiaPhone.Memoria}GB");
+
+        Iphone ioshone = new Iphone("654321", "Iphone Model", "IMEI456", 128);
+
+        // Chamando métodos herdados da classe Smartphone nokia
+        ioshone.Ligar();
+        ioshone.ReceberLigacao();
+
+        // Chamando o método sobrescrito da classe Nokia
+        ioshone.InstalarAplicativo("MeuAppIphone");
+
+        // Você também pode acessar as propriedades herdados da classe Smartphone
+        Console.WriteLine($"Número: {ioshone.Numero}, Modelo: {ioshone.Modelo}, Memória: {ioshone.Memoria}GB");
+    }
+}
